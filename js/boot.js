249c04a3093c92f3320f4167513eec17c84ffade
Irul24 <fajrichoirul73@gmail.com>
2024-08-06 09:23:54 +0700

    second commit

--- js/boot.js
+++ js/boot.js
@@ -1,20 +1,25 @@
+// importing dojo&esri library & widget module 
 require([
+    // Esri Library
     "esri/config",
     "esri/WebMap",
     "esri/views/MapView",
-    "dojo/domReady",
+
+    // Widget Module 
     "bufferRadius/bufferRadius",
     "layer/LayerList",
     "menu/menuBar",
     "utils/home",
+    
+    // dojo library
+    "dojo/domReady",
     "dojo/parser", 
     "dijit/layout/BorderContainer",
     "dijit/layout/ContentPane"
-], function(Config, WebMap, MapView,ready,osx_buffer,Layer,menuBar,Osx_Home) {
+], function(Config, WebMap, MapView,osx_buffer,Layer,menuBar,Osx_Home,ready) {
     ready(function () {
         const webmap = new WebMap({
             portalItem: {
-                // autocasts as new PortalItem()
                 id: "60994025213a4874bdb2e39503c72407"
             }
         });
@@ -22,9 +27,11 @@ require([
         var view = new MapView({
             container: "map",
             map: webmap,
-            center: [106.840837, -6.294844], // longitude, latitude
+            center: [106.840837, -6.294844],
         zoom: 16
         });
+
+        // call module widget
         var bufferRadius = new osx_buffer(view);
         var layerList = new Layer(view);
         var menus = new menuBar();
